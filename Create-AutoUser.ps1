12b253fb0bcb9ee4ceb5405cf66624e64da3560a
Bmo1992 <brandonseahorse@gmail.com>
2018-09-11 16:30:32 -0700

    Working on user information check

--- Create-AutoUser.ps1
+++ Create-AutoUser.ps1
@@ -14,9 +14,6 @@
       .\Create-AutoUser.ps1 -FirstName John -LastName Smith -TemplateUser jdoe
 #>
 
-# Required modules to run the cmdlets in the script
-Import-Module ActiveDirectory
-
 # Parameters to be passed to the script
 [CmdletBinding()]
 Param
@@ -37,18 +34,30 @@ if(-not $principal.IsInRole($role))
     throw "This script requires elevated permissions, please confirm youre running from an elevated powershell prompt as an administrator"
 }
 
+# Required modules to run the cmdlets in the script
+Import-Module ActiveDirectory
+
 ######### VARIABLES ##########
 $display_name = "$FirstName $LastName"
 $samaccountname = "$($FirstName[0])$LastName"
 $temp_password = ConvertTo-SecureString -String "ABcd1234*" -AsPlainText -Force
 $template = Get-ADUser -Identity $TemplateUser
+$get_ad_groups = Get-ADPrincipalGroupMembership -Identity $TemplateUser | Select -ExpandProperty name
 
 $final_check = @{
     "ldap"=$samaccountname
     "parentou"=(($template).distinguishedname -replace '^.+?,(CN|OU.+)','$1')
 }
 
-Write-Host "Are you sure you want to create user $final_check.ldap in $final_check.parentou ?"
+Write-Host "Are you sure you want to create a new user with the following properties?"
+Write-Host $null
+Write-Host "Name = $FirstName $LastName"
+Write-Host "LDAP = $($final_check.ldap)"
+Write-Host "Parent OU = $($final_check.parentou)"
+Write-Host $null
+Write-Host "Security Group Membership =>"
+$get_ad_groups
+Write-Host $null
 
 # New-ADUser -Name $samaccountname -NewPassword $temppassword
 
