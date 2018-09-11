1258d63330ee6949bc505379996b82b879f0837e
Bmo1992 <brandonseahorse@gmail.com>
2018-09-11 11:24:16 -0700

    Created new script Create-AutoUser.ps1

---
+++ Create-AutoUser.ps1
@@ -0,0 +1,33 @@
+#!/bin/pwsh
+<#
+#  Author  : BMO
+#  Purpose : To automate a user creation process that's become increasingly time consuming
+#            as more and more departments, users, and applications are added to a large
+#            network. 
+#  Created : September 11, 2018		 
+#>
+
+<#
+    .SYNOPSIS
+      Create-AutoUser
+    .EXAMPLE
+      .\Create-AutoUser.ps1
+#>
+
+
+Param
+(
+    [Parameter(Mandatory=$True)]$FirstName,
+    [Parameter(Mandatory=$True)]$LastName,
+    [Parameter(Mandatory=$True)]$TemplateUser
+)
+
+$displayname = "$FirstName $LastName"
+$samaccountname = "$($FirstName[0])$LastName"
+$temppassword = ConvertTo-SecureString -String "ABcd1234*" -AsPlainText -Force
+$template = Get-ADUser -Identity $TemplateUser
+
+New-ADUser -Name $samaccountname -NewPassword $temppassword
+
+
+
