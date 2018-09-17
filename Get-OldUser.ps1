efd25e7960c5cb9e87d747cadecb84eff2faf800
Bmo1992 <brandonseahorse@gmail.com>
2018-09-17 08:54:18 -0700

    Added a function to check for admin priviledges to ensure the user will have the rights to pull the enabled property from AD

--- Get-OldUser.ps1
+++ Get-OldUser.ps1
@@ -6,7 +6,7 @@
 
 <#
     .SYNOPSIS
-      Get-OldComputer.ps1 is a script to quickly audit a list of all users who have not signed into the domain in over 30 days.
+      Get-OldUser.ps1 is a script to quickly audit a list of all users who have not signed into the domain in over 30 days.
     .EXAMPLE
 #>
 
@@ -16,6 +16,12 @@ Param
 
 )
 
+# Check to confirm this script is being run by an admin in an elevated powershell prompt or else exit. If run from a non-priviledged
+# prompt then powershell will be unabled to read the Enabled AD property thanks to UAC
+$identity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+$principal = [System.Security.Principal.WindowsPrincipal] $identity
+$role = [System.Security.Principal.WindowsBuiltInRole] "Administrator"
+
 # Required module to run the cmdlets in the script
 Import-Module ActiveDirectory
 
@@ -23,7 +29,7 @@ Import-Module ActiveDirectory
 $daysback = "-30"
 $current_date = Get-Date
 $month_old = $current_date.AddDays($daysback)
-
+$all_user_objects = Get-ADUser -Filter * -Properties * 
 
 ForEach( $user in $all_user_objects) 
 {
