b18bdeacfb7d1509a5fd042e5b417386f2b0b809
Bmo1992 <brandonseahorse@gmail.com>
2018-09-19 10:59:01 -0700

    Moved the Invoke-ExecutionPolicy cmdlet into the DomainController section of the script

--- ConnectTo-RemoteServer.ps1
+++ ConnectTo-RemoteServer.ps1
@@ -37,7 +37,6 @@ Param
 )
 
 $user_credential = Get-Credential
-Invoke-ExecutionPolicy Unrestricted -Force
 
 
 if($Exchange)
@@ -48,6 +47,7 @@ if($Exchange)
 }
 elseif($DomainController -ne $null)
 {
+    Invoke-ExecutionPolicy Unrestricted -Force
     $Session = New-PSSession -ComputerName $Hostname -Credential $user_credential -Authentication Kerberos
     Invoke-Command $Session -ScriptBlock { Import-Module ActiveDirectory,GroupPolicy }
     Import-PSSession $Session -module ActiveDirectory,GroupPolicy
