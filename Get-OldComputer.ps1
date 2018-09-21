9025bfabf93df62b3c358196b5df3554f50cade9
Bmo1992 <brandonseahorse@gmail.com>
2018-09-21 10:23:36 -0700

    Added an export to csv capability thats specified as an argument of the cmdlet and added the computer OS as part of the output

--- Get-OldComputer.ps1
+++ Get-OldComputer.ps1
@@ -3,19 +3,24 @@
 #  Purpose : Find all computer objects that havn't checked into Active Directory for 30 or more days to 
 #            identify potentially retired computers that can be removed or disabled.
 #  Created : September 16, 2018
-#  Status  : Unfinished
+#  Updated : September 21, 2018
+#  Status  : Functional
 #>
 
 <#
     .SYNOPSIS
       Get-OldComputer.ps1 is a script to help assist in AD cleanup by identifying all computer objects that have not checked in for 30 or more days.
     .EXAMPLE
+      ./Get-OldComputer.ps1
+    .EXAMPLE
+      ./Get-OldComputer.ps1 -ExportCSV C:\Users\jdoe\Documents\user_export.csv
 #>
 
 [CmdletBinding()]
 Param
 (
-
+    [Parameter()]
+    [string]$ExportCSV
 )
 
 # Required module to run the cmdlets in the script
@@ -26,23 +31,41 @@ $daysback = "-30"
 $current_date = Get-Date
 $month_old = $current_date.AddDays($daysback)
 $current_date_string = Get-Date -DisplayHint Date | Out-String
-$all_computer_objects = Get-ADComputer -Filter * -Properties * | Select Name,LastLogonDate
+$all_computer_objects = Get-ADComputer -Filter * -Properties * | ?{ $_.Enabled -eq $True }
 
 
-<#
-$time_compare = [ordered]@{
-    "Day" = $CurrentDateString | %{ $_.Split(' ')[2] -Replace',',''; }
-    "Month" = $CurrentDateString | %{ $_.Split(' ')[1]; }
-    "Year" = $CurrentDateString | %{ $_.Split(',')[2] -Replace' ',''; }
-}
-#>
-
-ForEach( $computer in $all_computer_objects) 
+$computer_list_scrubed = ForEach($computer in $all_computer_objects) 
 {
     if( $computer.LastLogonDate -lt $month_old )
     {
-        Write-Output $computer.Name
+        $computer
     }
 }
 
+$computer_paramcheck = $computer_list_scrubbed | Select-Object `
+    @{
+        Expression={
+            $_.Name
+        };
+        Label="Name"
+    },
+    @{
+        Expression={
+            $_.LastLogonDate
+        };
+        Label="Last Logon"
+    },
+        Expression={
+            $_.OperatingSystem
+        };
+        Label="Operating System"
+    }
 
+if($ExportCSV -ne $null)
+{
+    $computer_paramcheck | Export-CSV $ExportCSV
+}
+else
+{
+    $computer_paramcheck | Format-Table
+}
