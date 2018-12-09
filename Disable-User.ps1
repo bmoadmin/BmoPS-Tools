b3da9097a47087eab5b1162dcfca8e563d5587cd
Bmo1992 <brandonseahorse@gmail.com>
2018-12-09 14:42:16 -0800

    Completed the main portion of script. Commented each portion. Script remains untested

--- Disable-User.ps1
+++ Disable-User.ps1
@@ -1,7 +1,8 @@
 <#
 #  Author  : BMO
-#  Purpose : Automate the disabling of AD Users 
+#  Purpose : Automate the disabling of AD Users.
 #  Created : September 22, 2018
+#  Updated : December 9, 2018
 #  Status  : Unfinished
 #>
 
@@ -12,15 +13,17 @@
       .\Disable-User.ps1 -Identity jdoe -DomainName bigcompany.local -DisabledOU "Terminated Users"
 #>
 
-# Parameters to be passed to the script
+# Parameters to be passed to the script.
 [CmdletBinding()]
 Param
 (
     [Parameter(
         Mandatory=$True
     )]
-    [string]$Identity,
+    [string]$Users,
+    [string]$DisabledPassword,
     [string]$DomainName,
+    [string]$DisabledUserGroup,
     [string]$DisabledOU
 )
 
@@ -35,15 +38,49 @@ if(-not $principal.IsInRole($role))
     throw "This script requires elevated permissions, please confirm youre running from an elevated powershell prompt as an administrator"
 }
 
-# Required modules to run the cmdlets in the script
+# Required modules to run the cmdlets in the script.
 Import-Module ActiveDirectory
+
 
 ######### VARIABLES ##########
 
-$disabled_password = ConvertTo-SecureString -String "Terminated-1982" -AsPlainText -Force
-$get_ad_groups = Get-ADPrincipalGroupMembership -Identity $TemplateUser | Select -ExpandProperty name
+$disabled_password = ConvertTo-SecureString -String $DisabledPassword -AsPlainText -Force
+$disabled_ou_dn = (Get-ADOrganizationalUnit -Filter * | ?{ $_.Name -eq $DisabledOU }).DistinguishedName
+
 
 ########### MAIN ############
 
-Disable-ADACcount -Identity $Identity
+# Loop through each element of the Users array which is passed to the script as an argument. 
+ForEach($user in $Users)
+{
+    # Get the distinguished name of the user, this will be needed to operate on later in the script.
+    $user_distinguishedname = (Get-ADUser $user).DistinguishedName
 
+    # Reset the user's password to the disabled password passed to the script by the DisabledPassword argument 
+    Set-ADAccountPassword -Identity $user -NewPassword $disabled_password -Reset
+
+
+    # Add the user to the Disabled user group passed to the script by the DisabledUserGroup argument. Get the sid of the disabled 
+    # user group, grab the last 4 numbers in the sid and then set disabled user group as the user's primary group. 
+    Add-ADGroupMember -Identity $DisabledUserGroup -Members $user
+    $primary_group_sid = (Get-ADGroup $DisabledUserGroup).Sid
+    [int]$primary_group_id = $primary_group_sid.Substring($primary_group_sid.LastIndexOf("-")+1)
+    Set-ADObject -Identity $(Get-ADUser $user | Select -ExpandProperty DistinguishedName) -Replace `
+    @{primaryGroupID="$primary_group_id"}
+
+
+    # Get all the groups a user is in besides the disabled user group and then remove them for all those other groups.
+    $groups = Get-ADUser $user -Properties * | Select -ExpandProperty MemberOf | ?{ `
+        $_ -notmatch $DisabledUserGroup
+    }
+    ForEach($group in $groups)
+    {
+        Remove-ADGroupMember -Identity $group -Members $user -Confirm:$False
+    }
+
+
+    # Finally disable the user and move them to the disabled user OU.
+    Disable-ADAccount -Identity $user
+    Move-ADObject -Identity $user_distinguishedname -TargetPath $disabled_ou_dn
+
+}
