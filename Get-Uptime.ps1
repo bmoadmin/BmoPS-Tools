77ab6ee4b8fea4aa742c324deb7325439c3dce45
Bmo1992 <brandonseahorse@gmail.com>
2018-12-10 11:28:36 -0800

    Added powershell help file information

--- Get-Uptime.ps1
+++ Get-Uptime.ps1
@@ -3,16 +3,32 @@
 #  Purpose : Get the uptime of a computer. By default it pulls the local uptime but a remote PC can be specified with the 
 #            -ComputerName argument.
 #  Created : October 25, 2018
+#  Updated : December 10, 2018
 #  Status  : Finished
 #>
-
 <#
     .SYNOPSIS
-      Get-Uptime.ps1 is a script to get the local uptime of a PC or the uptime of a remote PC.
-    .Syntax
-      Get-Uptime.ps1 -ComputerName <string>
+      Check the uptime of a computer.
+
+    .DESCRIPTION
+      Check the uptime of either the local computer or a remote computer by using the ComputerName argument. To get a remote computer
+      uptime it needs to be on the same domain.
+
+    .PARAMETER ComputerName
+      Specify a different computer other then the local one retrieve the uptime from.
+
+    .EXAMPLE
+      Get-Uptime.ps1
+
+      Get the uptime of the locahost.
+
     .EXAMPLE
       Get-Uptime.ps1 -ComputerName DC01
+
+      Get the uptime of the remote computer with the hostname DC01.
+
+    .NOTES
+      github.com/Bmo1992
 #>
 
 [CmdletBinding()]
