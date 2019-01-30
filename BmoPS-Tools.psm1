bb2664fd400c56e8c562f4bca523831bc48dbbea
Bmo1992 <brandonseahorse@gmail.com>
2019-01-29 18:52:35 -0800

    updated the psmodule file

--- BmoPS-Tools.psm1
+++ BmoPS-Tools.psm1
@@ -11,3 +11,16 @@
       CREATED : January 26, 2019
 #>
 
+$Functions = @(Get-ChildItem -Path $PSScriptRoot\Functions\*.ps1 -ErrorAction SilentlyContinue)
+
+ForEach ($script_function in $Functions)
+{
+    Try
+    {
+        . $script_function.fullname
+    }
+    Catch
+    {
+        Write-Error -Message "Failed to import function $($script_function.fullname): $_"
+    }
+}
