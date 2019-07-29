b508d8e133e84a91c269f075f5d7b1bc4f94e2b6
Bmo1992 <brandonseahorse@gmail.com>
2019-07-29 12:15:52 -0700

    Updated ps module file to include new Public repo path

--- BmoPS-Tools.psm1
+++ BmoPS-Tools.psm1
@@ -1,7 +1,9 @@
 <#
     .SYNOPSIS
+      BmoPS-Tools is a random assortment of scripts that Ive created to make daily tasks easier.
 
     .DESCRIPTION
+      This is the PS module file for my ps repository with a collection of assorted scripts for daily admin tasks.
 
     .NOTES
       NAME    : BmoPS-Tools
@@ -11,16 +13,16 @@
       CREATED : January 26, 2019
 #>
 
-$Functions = @(Get-ChildItem -Path $PSScriptRoot\Functions\*.ps1 -ErrorAction SilentlyContinue)
+$Public = @(Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue)
 
-ForEach ($script_function in $Functions)
+ForEach ($function in $Public)
 {
     Try
     {
-        . $script_function.fullname
+        . $function.fullname
     }
     Catch
     {
-        Write-Error -Message "Failed to import function $($script_function.fullname): $_"
+        Write-Error -Message "Failed to import function $($function.fullname): $_"
     }
 }
